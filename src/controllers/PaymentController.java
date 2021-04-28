50938d57e170fcc3a29571350da527697194dbed
mrtryharder2 <thomasmunguya@gmail.com>
2021-04-28 23:59:41 +0200

    Added PaymentController class

---
+++ src/controllers/PaymentController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author MTH
+ */
+public class PaymentController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+    
+    /**
+     * Setups event handlers for number pad buttons
+     */
+    public void setUpNumberPadEventHandlers() {
+        for(int i = 0; i < 1) {
+            
+        }
+    }
+    
+    
+}
