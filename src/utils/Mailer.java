b4a8ecdca08a7b22f41e74aae3ffe035b715e0f8
mrtryharder2 <thomasmunguya@gmail.com>
2021-04-29 00:02:05 +0200

    Added Mailer class

---
+++ src/utils/Mailer.java
@@ -0,0 +1,93 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package utils;
+//
+//import com.sun.mail.smtp.SMTPMessage;
+//import java.io.File;
+//import java.io.IOException;
+//import java.util.*;
+//import javax.mail.Message;
+//import javax.mail.MessagingException;
+//import javax.mail.Multipart;
+//import javax.mail.Transport;
+//import javax.mail.internet.InternetAddress;
+//import javax.mail.internet.MimeBodyPart;
+//import javax.mail.internet.MimeMultipart;
+//import main.Movie;
+//import payment.Receipt;
+//
+///**
+// *Helper class for sending email
+// */
+//public class Mailer {
+//   
+//    private final String host = "smtp.gmail.com";
+//    private final String port = "587";
+//    private final Properties props = System.getProperties();
+//    private final String FROM = "thomas.munguya@gmail.com";
+//    private final String PASSWORD = "thomasThomas2424";
+//    private static Mailer mailer;
+//    
+//    public Mailer() {
+//        
+//    }
+//    
+//    private void sendEmail(String destinationEmail, String content) {
+//        
+//        props.put("mail.smtp.host", host);
+//        props.put("mail.smtp.user", FROM);
+//	props.put("mail.smtp.password", PASSWORD);
+//	props.put("mail.smtp.port", port);
+//	props.put("mail.smtp.auth", "true");
+//	props.put("mail.smtp.starttls.enable", "true");
+//        javax.mail.Session session = javax.mail.Session.getDefaultInstance(props);
+//        
+//        MimeBodyPart p1 = new MimeBodyPart();
+//	Multipart mp = new MimeMultipart();
+//	MimeBodyPart imageBodyPart = new MimeBodyPart();
+//        
+//        try {
+//            mp.addBodyPart(p1);
+//            p1.setText("");
+//            SMTPMessage message = new SMTPMessage(session);
+//            message.setFrom(new InternetAddress(FROM));
+//            message.addRecipient(Message.RecipientType.TO, new InternetAddress(destinationEmail));
+//            message.setContent(mp);
+//            Transport transport = session.getTransport("smtp");
+//            transport.connect(host, FROM, PASSWORD);
+//            transport.sendMessage(message, message.getAllRecipients());
+//            transport.close();
+//	} catch (MessagingException ex) {
+//            ex.printStackTrace();
+//	}
+//        
+//        
+//    }
+//    
+//    public static Mailer getInstance() {
+//        if(mailer == null) {
+//            return new Mailer();
+//        }
+//        return mailer;
+//    }
+//    
+//    public void sendNewReleasesEmail(String destinationEmail,  List<Movie> newReleases) {
+//        //to be implemented
+//    }
+//    
+//    public void sendReceiptEmail(String destinationEmail, Receipt receipt) throws IOException {
+//        final File RECEIPT_HTML_FILE = new File("utils/receipt_html.txt");
+//        Scanner scanner = new Scanner(RECEIPT_HTML_FILE);
+//        StringBuilder receiptHTML = new StringBuilder();
+//        while(scanner.hasNextLine()) {
+//            receiptHTML.append(scanner.nextLine() + "\n");
+//        }
+//        
+//        sendEmail(destinationEmail, receiptHTML.toString());
+//        
+//    }
+//    
+//}
