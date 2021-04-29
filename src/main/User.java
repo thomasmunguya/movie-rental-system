61764dfeaf7aeb758f988a3195084b6522d2787e
mrtryharder2 <thomasmunguya@gmail.com>
2021-04-29 13:06:52 +0200

    Implemented the main.User.retrieveOne() method

--- src/main/User.java
+++ src/main/User.java
@@ -1,16 +1,16 @@
 package main;
 
-import payment.PaymentCard;
 import database.*;
 import static database.DatabaseAccessor.CONNECTION;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.*;
+import payment.*;
 
 /**
  * Models a user
  * A user has a payment card and an optional email address
  */
-public class User implements Persistable {
+public class User implements Persistable, Retrievable {
 
     private String emailAddress;
     private PaymentCard paymentCard;
@@ -90,5 +90,50 @@ public class User implements Persistable {
         }
         return false;
     }
-  
+
+    @Override
+    public Retrievable retrieveOne(String columnName, String columnValue) {
+       final String QUERY = "SELECT * FROM user WHERE " + columnName + 
+                " = '" + columnValue + "';";
+        try {
+            Statement statement = CONNECTION.createStatement();
+            ResultSet retrievedUserRS = statement.executeQuery(QUERY);
+            if(retrievedUserRS.next()) {
+                User user = new User();
+                user.setEmailAddress(retrievedUserRS.getString("email"));
+                if(PaymentCard.retrievePaymentCardType(retrievedUserRS.
+                        getString("payment_card_number")).equals(PaymentCardType
+                                .CREDIT_CARD.toString())) {
+                    CreditCard cCard = new CreditCard();
+                    cCard.setCardNumber(retrievedUserRS.getString("payment_card_number"));
+                    cCard = (CreditCard) cCard.retrieveOne("card_number",
+                            retrievedUserRS.getString("payment_card_number"));
+                    user.setPaymentCard(cCard);
+                    return user;
+                } else if(PaymentCard.retrievePaymentCardType(retrievedUserRS.
+                        getString("payment_card_number")).equals(PaymentCardType
+                                .DEBIT_CARD.toString())) {
+                    DebitCard dCard = new DebitCard();
+                    dCard.setCardNumber(retrievedUserRS.getString("payment_card_number"));
+                    dCard = (DebitCard) dCard.retrieveOne("card_number",
+                            retrievedUserRS.getString("payment_card_number"));
+                    user.setPaymentCard(dCard);
+                    return user;
+                }
+               
+            }
+        }catch(SQLException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+    
+    
+
+    @Override
+    public List<Retrievable> retrieveAll() {
+       return null;
+    }
+    
+   
 }
